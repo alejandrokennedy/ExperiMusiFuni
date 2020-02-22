2ba643398967180d4447bd28041dfa5f6ba89b3f
alejandrokennedy <alejandrokennedy@gmail.com>
2020-02-22 17:08:39 -0500

    more cleaning up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import * as d3 from 'd3'
 import { midi, transpose, scale } from "tonal"
-// import _ from 'lodash'
 
 
 // ************************
@@ -11,17 +10,6 @@ console.log(d3)
 console.log(midi("c4"))
 console.log(transpose("d4", "3M"))
 console.log(scale("major").map(transpose("C2")))
-
-// function component() {
-//   const element = document.createElement('div');
-
-//   // Lodash, currently included via a script, is required for this line to work
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-//   return element;
-// }
-
-// document.body.appendChild(component());
 
 
 // ************************
